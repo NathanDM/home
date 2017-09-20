8dd5c2a76e9f08fdc9022b37f1c56fc43e8ed79d
Baptiste Lecocq <hello@tiste.io>
2017-09-20 21:35:19 +0200

    feat: allow to blink

--- app.js
+++ app.js
@@ -1,3 +1,35 @@
 'use strict';
 
-import _ from 'lodash';
+import gpio from 'rpi-gpio';
+
+const pin = 11;
+let count = 0;
+const max = 3;
+const delay = 3000;
+
+gpio.setup(pin, gpio.DIR_OUT, on);
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function on() {
+    if (count >= max) {
+        gpio.destroy();
+        return;
+    }
+
+    await sleep(delay);
+
+    console.log('on');
+    gpio.write(pin, 1, off);
+    count += 1;
+}
+
+async function off() {
+
+    await sleep(delay);
+
+    console.log('off');
+    gpio.write(pin, 0, on);
+}
