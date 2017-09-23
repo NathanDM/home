e2ed19cf02ab511cdd318a637e089f3aad83adaf
Nathan DAMIE <nathan.damie@gmail.com>
2017-09-23 17:04:45 +0200

    test(joypad): listen x y joypad

--- app.js
+++ app.js
@@ -2,16 +2,42 @@
 
 import gpio from 'rpi-gpio';
 
-const pin = 11;
+const pinX = 25;
+const pinY = 8;
+
 let count = 0;
 const max = 3;
 const delay = 3000;
 
-gpio.setup(pin, gpio.DIR_OUT, on);
+// gpio.setup(pin, gpio.DIR_OUT, on);
+// gpio.setup(pinX, gpio.DIR_IN, listenX);
+gpio.setup(pinY, gpio.DIR_IN, readInput);
 
 function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+async function listenX(x){
+    console.log('listen X : ', x)
+}
+async function listenY(y){
+    console.log('listen Y : ', y)
+}
+
+
+gpio.setup(pinX, gpio.DIR_IN, gpio.EDGE_BOTH);
+gpio.on('change', function(channel, value) {
+    console.log('Channel ' + channel + ' value is now ' + value);
+});
+
+
+function readInput() {
+    gpio.read(pinY, function(err, value) {
+        console.log('The value is ' + value);
+    });
+}
+
+gpio.setup(7, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 async function on() {
     if (count >= max) {
@@ -21,7 +47,7 @@ async function on() {
 
     await sleep(delay);
 
-    console.log('on');
+    console.log('onskfldsmk');
     gpio.write(pin, 1, off);
     count += 1;
 }
